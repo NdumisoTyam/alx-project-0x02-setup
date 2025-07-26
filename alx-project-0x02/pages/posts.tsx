69fa54271fc44c49f43df1dcc2ab8c2ec02fcfb2
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-26 17:52:41 +0200

    Create posts.tsx

---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/layout/Header";
+
+export default function PostsPage() {
+  return (
+    <>
+      <Header />
+      <main className="p-8">
+        <h1 className="text-3xl font-bold">Posts</h1>
+        <p>Here’s where your blog posts or articles will go.</p>
+      </main>
+    </>
+  );
+}
