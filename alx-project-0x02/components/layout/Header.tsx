3cf0ea0616976e48f4ea3754e0a5f08be365492a
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-26 16:06:16 +0200

    Delete alx-project-0x02/components directory

--- alx-project-0x02/components/layout/Header.tsx
+++
@@ -1,7 +0,0 @@
-const Header = () => (
-  <header className="bg-blue-600 text-white p-4 text-center">
-    <h1 className="text-2xl font-bold">Welcome to ALX Project 2</h1>
-  </header>
-);
-
-export default Header;
