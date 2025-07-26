18edb912114ae9e8859752051796209f12c12f5e
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-26 17:34:26 +0200

    Update Header.tsx

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -5,3 +5,20 @@ const Header = () => (
 );
 
 export default Header;
+
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <nav className="flex gap-6">
+        <Link href="/home" className="hover:underline">
+          Home
+        </Link>
+        <Link href="/about" className="hover:underline">
+          About
+        </Link>
+      </nav>
+    </header>
+  );
+}
