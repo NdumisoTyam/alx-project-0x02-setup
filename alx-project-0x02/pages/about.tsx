8f301bd7510bb592f27b4cec9205f3d2dbee897c
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-26 17:33:32 +0200

    Create about.tsx

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div className="min-h-screen bg-white p-8">
+      <h1 className="text-3xl font-bold text-gray-800">About Us</h1>
+    </div>
+  );
+}
