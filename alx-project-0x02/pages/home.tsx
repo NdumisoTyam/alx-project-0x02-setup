226b62a1204faa8b0a47293eb65dd423ca0b8939
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-07-26 17:33:03 +0200

    Create home.tsx

---
+++ alx-project-0x02/pages/home.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen bg-white p-8">
+      <h1 className="text-3xl font-bold text-gray-800">Welcome to the Home Page</h1>
+    </div>
+  );
+}
